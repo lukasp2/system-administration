374419bfb744adb218342fdf614f7268bb1f6add
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-21 19:14:53 +0100

    Update router_script.sh

--- router_script.sh
+++ router_script.sh
@@ -4,13 +4,13 @@ echo "1" > /proc/sys/net/ipv4/ip_forward
 
 apt-get -y install iptables-persistent
 
-/sbin/iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
-/sbin/iptables -A FORWARD -i ens3 -o ens4 -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -A FORWARD -i ens4 -o ens3 -j ACCEPT
+echo "/sbin/iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE" >> ~/.bashrc
+echo "/sbin/iptables -A FORWARD -i ens3 -o ens4 -m state --state RELATED,ESTABLISHED -j ACCEPT" >> ~/.bashrc
+echo "/sbin/iptables -A FORWARD -i ens4 -o ens3 -j ACCEPT" >> ~/.bashrc
 
 echo "dhclient ens3 -v" >> ~/.bashrc
 echo "echo '1' > /proc/sys/net/ipv4/ip_forward" >> ~/.bashrc
 
-# cat ~/system-administration/router/\\etc\\resolv.conf > /etc/resolve.conf
+cat ~/system-administration/router/\\etc\\resolv.conf > /etc/resolve.conf
 
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
