780fe0d1f29bde05649fa02cdcb6cad6b3feb71e
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-21 11:48:51 +0100

    Update server_script.sh

--- server_script.sh
+++ server_script.sh
@@ -7,12 +7,13 @@ apt-get -y dist-upgrade
 apt-get -y install bind9 bind9utils bind9-doc
 apt-get -y install dnsutils
 
-cat ./resources/server/\\etc\\network\\interfaces > /etc/network/interfaces
-cat ./resources/server/\\etc\\bind\\named.conf.local > /etc/bind/named.conf.local
-cat ./resources/server/\\etc\\bind\\for.dons.com > /etc/bind/for.dons.com
-cat ./resources/server/\\etc\\bind\\rev.dons.com > /etc/bind/rev.dons.com
-cat ./resources/server/\\etc\\resolv.conf > /etc/resolv.conf
-cat ./resources/server/\\etc\\bind\\named.conf.options > /etc/bind/named.conf.options
+cat ~/system-administration/resources/server/\\etc\\network\\interfaces > /etc/network/interfaces
+cat ~/system-administration/resources/server/\\etc\\bind\\named.conf > /etc/bind/named.conf
+cat ~/system-administration/resources/server/\\etc\\bind\\named.conf.local > /etc/bind/named.conf.local
+cat ~/system-administration/resources/server/\\etc\\bind\\for.dons.com > /etc/bind/for.dons.com
+cat ~/system-administration/resources/server/\\etc\\bind\\rev.dons.com > /etc/bind/rev.dons.com
+cat ~/system-administration/resources/server/\\etc\\resolv.conf > /etc/resolv.conf
+cat ~/system-administration/resources/server/\\etc\\bind\\named.conf.options > /etc/bind/named.conf.options
 
 apt-get update
 /etc/init.d/bind9 restart
