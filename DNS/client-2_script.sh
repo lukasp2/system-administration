fd4f0b25c6cd37d0b40014487eef2b680021ae4a
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-21 19:54:31 +0100

    Update client-2_script.sh

--- DNS/client-2_script.sh
+++ DNS/client-2_script.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get -y upgrade
 
-cat ./resources/client-2/\\etc\\network\\interfaces > /etc/network/interfaces
-cat ./resources/client-2/\\etc\\resolv.conf > /etc/resolv.conf
+cat ~/system-architecture/DNS/client-2/\\etc\\network\\interfaces > /etc/network/interfaces
+cat ~/system-architecture/DNS/client-2/\\etc\\resolv.conf > /etc/resolv.conf
