50faef3023df9b1a678d27cef7401426ecc1dd8d
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-21 19:54:04 +0100

    Update client-1_script.sh

--- DNS/client-1_script.sh
+++ DNS/client-1_script.sh
@@ -1,5 +1,5 @@
 apt-get update
 apt-get -y upgrade
 
-cat ./resources/client-1/\\etc\\network\\interfaces > /etc/network/interfaces
-cat ./resources/client-1/\\etc\\resolv.conf > /etc/resolv.conf
+cat ~/system-architecture/DNS/client-1/\\etc\\network\\interfaces > /etc/network/interfaces
+cat ~/system-architecture/DNS/client-1/\\etc\\resolv.conf > /etc/resolv.conf
