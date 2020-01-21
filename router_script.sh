ffc2101f8f43b81019641f65cdf97b3089034e84
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-21 10:51:34 +0100

    Update router_script.sh

--- router_script.sh
+++ router_script.sh
@@ -4,7 +4,7 @@ echo "1" > /proc/sys/net/ipv4/ip_forward
 
 apt-get -y install iptables-persistent
 
-/sbin/iptables -t nat -A POSTROUTING -o ens -j MASQUERADE
+/sbin/iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
 /sbin/iptables -A FORWARD -i ens3 -o ens4 -m state --state RELATED,ESTABLISHED -j ACCEPT
 /sbin/iptables -A FORWARD -i ens4 -o ens3 -j ACCEPT
 
