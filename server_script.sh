2a98733e520d3c78d6581389b8723b9fc036665b
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-21 09:15:03 +0100

    Update server_script.sh

--- server_script.sh
+++ server_script.sh
@@ -7,12 +7,12 @@ apt-get -y dist-upgrade
 apt-get -y install bind9 bind9utils bind9-doc
 apt-get -y install dnsutils
 
-cat ./server/\etc\network\interfaces > /etc/network/interfaces
-cat ./server/\etc\bind\named.conf.local > /etc/bind/named.conf.local
-cat ./server/\etc\bind\for.dons.com > /etc/bind/for.dons.com
-cat ./server/\etc\bind\rev.dons.com > /etc/bind/rev.dons.com
-cat ./server/\etc\resolv.conf > /etc/resolv.conf
-cat ./server/\etc\bind\named.conf.options > /etc/bind/named.conf.options
+cat ./resources/server/\etc\network\interfaces > /etc/network/interfaces
+cat ./resources/server/\etc\bind\named.conf.local > /etc/bind/named.conf.local
+cat ./resources/server/\etc\bind\for.dons.com > /etc/bind/for.dons.com
+cat ./resources/server/\etc\bind\rev.dons.com > /etc/bind/rev.dons.com
+cat ./resources/server/\etc\resolv.conf > /etc/resolv.conf
+cat ./resources/server/\etc\bind\named.conf.options > /etc/bind/named.conf.options
 
 apt-get update
 /etc/init.d/bind9 restart
