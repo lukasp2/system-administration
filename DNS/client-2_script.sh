c78a1b8a378e32ac62a93c2f9094f31e4b5ebe6f
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 09:28:47 +0100

    Delete client-2_script.sh

--- DNS/client-2_script.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt-get -y upgrade
-
-cat ~/system-architecture/DNS/client-2/\\etc\\network\\interfaces > /etc/network/interfaces
-cat ~/system-architecture/DNS/client-2/\\etc\\resolv.conf > /etc/resolv.conf
