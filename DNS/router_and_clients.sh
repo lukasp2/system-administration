402d19b8f4a3648adf59d789594d439f6ba6a792
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 16:08:53 +0100

    Update router_and_clients.sh

--- DNS/router_and_clients.sh
+++ DNS/router_and_clients.sh
@@ -3,8 +3,7 @@
 apt-get update
 apt-get -y upgrade
 
-cat ~/system-administration/DNS/server/\\etc\\resolv.conf > /etc/resolv.conf
-
-ifdown ens3; ifup ens3
+#cat ~/system-administration/DNS/server/\\etc\\resolv.conf > /etc/resolv.conf
+#ifdown ens3; ifup ens3
 
 
