198f4b84013c4469e98ad239ff8f7caa0c84f071
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 09:39:02 +0100

    Update and rename router_script.sh to router_and_clients.sh

--- DNS/router_and_clients.sh
+++ DNS/router_and_clients.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+apt-get update
+apt-get -y upgrade
+
 cat ~/system-administration/DNS/server/\\etc\\resolv.conf > /etc/resolv.conf
 cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf.options > /etc/bind/named.conf.options
