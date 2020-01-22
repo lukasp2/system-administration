84df892eda5a426f2aaa1cd3c510cdf27e44cf83
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 09:37:44 +0100

    Update router_script.sh

--- DNS/router_script.sh
+++ DNS/router_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cat ~/system-administration/DNS/router/\\etc\\resolv.conf > /etc/resolv.conf
+cat ~/system-administration/DNS/server/\\etc\\resolv.conf > /etc/resolv.conf
+cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf.options > /etc/bind/named.conf.options
