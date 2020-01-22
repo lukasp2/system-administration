ceba39600db4f559991ecadb12413fc750091e4d
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 09:39:15 +0100

    Delete clients_script.sh

--- DNS/clients_script.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt-get -y upgrade
-
-cat ~/system-architecture/DNS/server/\\etc\\resolv.conf > /etc/resolv.conf
-cat ~/system-architecture/DNS/server/\\etc\\bind\\named.conf > /etc/resolv.conf
