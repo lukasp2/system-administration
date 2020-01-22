8c5a6a0a418adce72ad3bcd639de64fd4e657386
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 15:17:42 +0100

    Update router_and_clients.sh

--- DNS/router_and_clients.sh
+++ DNS/router_and_clients.sh
@@ -3,11 +3,15 @@
 apt-get update
 apt-get -y upgrade
 
-mkdir /etc/bind/
-
 cat ~/system-administration/DNS/server/\\etc\\resolv.conf > /etc/resolv.conf
-cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf > /etc/bind/named.conf
-cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf.options > /etc/bind/named.conf.options
+
+#mkdir /etc/bind/
+#cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf > /etc/bind/named.conf
+#cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf.options > /etc/bind/named.conf.options
+
+mkdir -p /etc/resolvconf/resolv.conf.d/
+echo "domain dons.com" > /etc/resolvconf/resolv.conf.d/base
+echo "10.0.0.2" > /etc/resolvconf/resolv.conf.d/base
 
 ifdown ens3; ifup ens3
 
