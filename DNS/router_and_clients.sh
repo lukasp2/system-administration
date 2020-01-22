0fc3e478915a428ab8b410a120d3d9efe6194561
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 10:04:22 +0100

    Update router_and_clients.sh

--- DNS/router_and_clients.sh
+++ DNS/router_and_clients.sh
@@ -3,5 +3,12 @@
 apt-get update
 apt-get -y upgrade
 
+mkdir /etc/bind/
+
 cat ~/system-administration/DNS/server/\\etc\\resolv.conf > /etc/resolv.conf
+cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf > /etc/bind/named.conf
 cat ~/system-administration/DNS/server/\\etc\\bind\\named.conf.options > /etc/bind/named.conf.options
+
+ifdown ens3; ifup ens3
+
+
