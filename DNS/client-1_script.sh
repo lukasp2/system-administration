0e4247295aa8f4906e8b05a30d42dbf2087da819
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-22 09:28:34 +0100

    Update and rename client-1_script.sh to clients_script.sh

--- DNS/client-1_script.sh
+++
@@ -1,5 +0,0 @@
-apt-get update
-apt-get -y upgrade
-
-cat ~/system-architecture/DNS/client-1/\\etc\\network\\interfaces > /etc/network/interfaces
-cat ~/system-architecture/DNS/client-1/\\etc\\resolv.conf > /etc/resolv.conf
