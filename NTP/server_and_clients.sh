52380cf27f595eccc4dd3c3ee7c088f2dbfd6f6e
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-23 12:12:26 +0100

    Update server_and_clients.sh

--- NTP/server_and_clients.sh
+++ NTP/server_and_clients.sh
@@ -2,10 +2,12 @@
 
 apt-get -y install ntpdate
 
+service ntp stop
+ntpdate 10.0.0.1
+service ntp start
+
 timedatectl set-ntp off
 
 apt-get -y install ntp
-
 echo "server gw prefer iburst" > /etc/ntp.conf
-
 service ntp restart
