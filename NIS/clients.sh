0419c937a4124df48c121fbc743b99b52ea5f767
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-23 18:22:11 +0100

    Update clients.sh

--- NIS/clients.sh
+++ NIS/clients.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#apt-get update
-#apt-get upgrade
-#apt-get -y install nis
+apt-get update
+apt-get upgrade
+apt-get -y install nis
 
-#echo "domain example.com server server.example.com" >> /etc/yp.conf
+echo "domain example.com server server.example.com" >> /etc/yp.conf
 
 sed -ir 's/^passwd:\s*.*$/passwd: compat nis/g' /etc/nsswitch.conf
 sed -ir 's/^group:\s*.*$/group: compat nis/g' /etc/nsswitch.conf
