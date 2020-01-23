b0bf3a790d922fff8923b55db268e18136063f23
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-23 14:17:47 +0100

    Update server.sh

--- NIS/server.sh
+++ NIS/server.sh
@@ -4,3 +4,8 @@ apt-get -y install nis
 
 cat ~/system-administration/NIS/files/\\etc\\default\\nis > /etc/default/nis 
 cat ~/system-administration/NIS/files/\\etc\\ypserv.securenets > /etc/ypserv.securenets 
+
+sed -i 's/MERGE_PASSWD=false/MERGE_PASSWD=true/g' /var/yp/Makefile
+sed -i 's/MERGE_GROUP=false/MERGE_GROUP=true/g' /var/yp/Makefile
+
+/usr/lib/yp/ypinit -m
