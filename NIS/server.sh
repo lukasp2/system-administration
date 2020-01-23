14d1ab9e3307bdae8acf0a80bc8d84bd27bf2190
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-23 14:01:42 +0100

    Update server.sh

--- NIS/server.sh
+++ NIS/server.sh
@@ -3,3 +3,4 @@
 apt-get -y install nis
 
 cat ~/system-administration/NIS/files/\\etc\\default\\nis > /etc/default/nis 
+cat ~/system-administration/NIS/files/\\etc\\ypserv.securenets > /etc/ypserv.securenets 
