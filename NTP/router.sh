c609f1bf5fcb2a4e92fe7258818069300d4c6d01
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-23 11:51:36 +0100

    Update router.sh

--- NTP/router.sh
+++ NTP/router.sh
@@ -5,7 +5,7 @@ apt-get -y install ntp
 
 cat ~/system-administration/NTP/files/\\etc\\ntp.conf > /etc/ntp.conf
 
+apt-get -y install ufw
+ufw allow from any to any port 123 proto udp
+
 service ntp restart
-
-
-
