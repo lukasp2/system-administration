1feb71c53c00832d0a99a5c7fef00f0df9550b11
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-24 09:44:43 +0100

    Update testing.sh

--- NIS/testing.sh
+++ NIS/testing.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# When testing NIS at this point, when you have no clients, you may need to use the ypbind command manually 
-# to bind to the server. The ypcat command is useful to read the contents of a NIS map. The ypwhich command 
-# shows which server the client is bound to.
-
+# The ypwhich command shows which server the client is bound to (should be return 10.0.0.2)
 ypwhich
+
+
