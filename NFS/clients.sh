b30f374f1e142e61ad872bc6b561add4849a178b
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-28 12:06:42 +0100

    Update clients.sh

--- NFS/clients.sh
+++ NFS/clients.sh
@@ -3,3 +3,11 @@
 apt-get -y install autofs
 
 apt-get -y install nfs-client
+
+# 3-3 Clients automatically mount /usr/local from the server at boot (not necessarily with /usr/local as mount point)
+sed -i '/automount/d' /etc/nsswitch.conf
+echo -e "automount:\tfiles nis" >> /etc/nsswitch.conf
+
+nisrestart
+
+/etc/init.d/autofs start
