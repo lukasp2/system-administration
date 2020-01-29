8673e5c14e7c76500d37d35cb986971595406642
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-01-29 19:59:30 +0100

    Update testing.sh

--- NFS/testing.sh
+++ NFS/testing.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# 4-2 Move one user's home directory to /home2/USERNAME and the other user's home directory to /home1/USERNAME.
+
+# 4-2 Create two new users.
+
+# [note] Ensure that no home directories remain in /home. Do not change the home directory location in the user database
+
+# 5-3 Verify that all users can log on to the client and that the correct home directories are mounted.
