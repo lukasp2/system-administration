645551c2ed253ecf50e141b2f329bf1fce5eb29d
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-02-04 18:03:25 +0100

    Update test.sh

--- NFS/test.sh
+++ NFS/test.sh
@@ -20,6 +20,21 @@ useradd -m -G users -p password -b /home2 user2
 # [note] Ensure that no home directories remain in /home. Do not change the home directory location in the user database
 
 # 5-3 Verify that all users can log on to the client and that the correct home directories are mounted.
+USER1=$(cat /etc/passwd | grep user1 | cut -d ":" -f1,6)
+USER2=$(cat /etc/passwd | grep user2 | cut -d ":" -f1,6)
+
+if [[ $USER1 == "user1:/home1/user1" ]]; then
+  echo "Home directory for user1 correct" 
+else
+  echo "Home directory for user1 is incorrect" 
+fi
+
+if [[ $USER2 == "user2:/home2/user2" ]]; then
+  echo "Home directory for user2 correct" 
+else
+  echo "Home directory for user2 is incorrect" 
+fi
+
 su user1
 USER=$(whoami)
 if [[ $USER == user1 ]]; then
@@ -28,22 +43,12 @@ else
   echo "Could not log in to user1"
 fi
 
-cd ~
-if [[ $(pwd) == "/home1" ]]; then
-  echo "Home directory for user1 is correctly set to /home1"
-fi
-
 su user2
 USER=$(whoami)
 if [[ $USER == user2 ]]; then
   echo "Successfully logged in to user2"
 else
   echo "Could not log in to user2"
-fi
-
-cd ~
-if [[ $(pwd) == "/home2" ]]; then
-  echo "Home directory for user1 is correctly set to /home2"
 fi
 
 su -
