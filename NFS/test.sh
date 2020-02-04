32ac022a3429c8241f03994ec195db924932a348
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-02-04 10:41:54 +0100

    Update test.sh

--- NFS/test.sh
+++ NFS/test.sh
@@ -13,7 +13,33 @@ useradd -m -G users -p password -b /home2 user2
 # [note] Ensure that no home directories remain in /home. Do not change the home directory location in the user database
 
 # 5-3 Verify that all users can log on to the client and that the correct home directories are mounted.
+su user1
+USER=$(whoami)
+if [[ $USER == user1 ]]; then
+  echo "Successfully logged in to user1"
+else
+  echo "Could not log in to user1"
+fi
 
+cd ~
+if [[ $(pwd) == "/home1" ]]; then
+  echo "Home directory for user1 is correctly set to /home1"
+fi
+
+su user2
+USER=$(whoami)
+if [[ $USER == user2 ]]; then
+  echo "Successfully logged in to user2"
+else
+  echo "Could not log in to user2"
+fi
+
+cd ~
+if [[ $(pwd) == "/home2" ]]; then
+  echo "Home directory for user1 is correctly set to /home2"
+fi
+
+su -
 
 # delete users
 userdel -r user1
