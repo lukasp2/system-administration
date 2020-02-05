2ade16d4bc1b25d2d9f6ae876bdff27d35441977
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-02-05 10:05:08 +0100

    Update testing.sh

--- NIS/testing.sh
+++ NIS/testing.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # The ypwhich command shows which server the client is bound to (should be return 10.0.0.2)
-ypwhich
+OUTPUT=$(ypwhich)
+if [[ $OUTPUT == "10.0.0.2" ]]; then
+  echo "OK: ypwhich"
+else
+  echo "FAILED: ypwhich"
+fi
 
 
