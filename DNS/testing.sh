3fc29b84a4d01fd62c13b4456512031fb8167409
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-02-05 09:40:47 +0100

    Update testing.sh

--- DNS/testing.sh
+++ DNS/testing.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
 echo "Testing $ dig dons.com"
-dig dons.com
+dig dons.com | grep -A 1 "ANSWER SECTION:"
+DNS=$(dig dons.com | grep -A 1 "ANSWER SECTION:" | grep -o "10.0.0.2")
+
+if [[ $DNS == "10.0.0.2" ]]; then
+  echo "OK: dig dons.com"
+else
+  echo "FAILED: dig dons.com"
+fi
 
 echo "Testing $ dig -x 10.0.0.2"
-dig -x 10.0.0.2
+dig -x 10.0.0.2 | grep -A 1 "ANSWER SECTION:"
+DNS=$(dig -x 10.0.0.2 | grep -A 1 "ANSWER SECTION:" | grep -o "pri.dons.com")
 
-echo "Testing $ dig +short @dons.com porttest.dns-oarc.net TXT"
-dig +short @dons.com porttest.dns-oarc.net TXT
+if [[ $DNS == "pri.dons.com" ]]; then
+  echo "OK: dig -x 10.0.0.2"
+else
+  echo "FAILED: dig -x 10.0.0.2"
+fi
 
 echo "Checking the DNS configuration files"
-echo "if nothing is printed below, it means DNS configuration is valid."
-named-checkconf /etc/bind/named.conf
-named-checkconf /etc/bind/named.conf.local
+OUTPUT=$(named-checkconf /etc/bind/named.conf)
+if [[ $OUTPUT == "" ]]; then
+  echo "OK: named-checkconf /etc/bind/named.conf"
+else
+  echo "FAILED: named-checkconf /etc/bind/named.conf"
+fi
 
-echo "Checking the zone files, these commands should return two lines each, the last one 'OK'."
-named-checkzone dons.com /etc/bind/for.dons.com
-named-checkzone dons.com /etc/bind/rev.dons.com
+OUTPUT=$(named-checkconf /etc/bind/named.conf.local)
+if [[ $OUTPUT == "" ]]; then
+  echo "OK: named-checkconf /etc/bind/named.conf.local"
+else
+  echo "FAILED: named-checkconf /etc/bind/named.conf.local"
+fi
+
+OUTPUT=$(named-checkzone dons.com /etc/bind/for.dons.com | tail -n 1)
+if [[ $OUTPUT == "OK" ]]; then
+  echo "OK: named-checkzone dons.com /etc/bind/for.dons.com"
+else
+  echo "FAILED: named-checkzone dons.com /etc/bind/for.dons.com"
+fi
+
+OUTPUT=$(named-checkzone dons.com /etc/bind/rev.dons.com | tail -n 1)
+if [[ $OUTPUT == "OK" ]]; then
+  echo "OK: named-checkzone dons.com /etc/bind/rev.dons.com"
+else
+  echo "FAILED: named-checkzone dons.com /etc/bind/rev.dons.com"
+fi
