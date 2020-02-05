221bc7fff52baa2dfb9935fc6a790b51fa57dc1d
Lukas Pohlman <45101415+lukasp2@users.noreply.github.com>
2020-02-05 10:02:37 +0100

    Update testing.sh

--- NTP/testing.sh
+++ NTP/testing.sh
@@ -4,8 +4,18 @@
 
 # Checks if the client machine’s time is synchronized with NTP server. 
 # The output should ideally show a time *offset* between the two systems.
-ntpdate -q gw
+NTPDATE=$(ntpdate -q gw | grep "server 10.0.0.1")
+if [[ $NTPDATE == "" ]]; then
+  echo "FAILED: ntpdate -q gw"
+else
+  echo "OK: ntpdate -q gw"
+fi
 
 # You can view the time synchronization queue by running the following command.
 # You should be able to see gw.example.som as the time synchronization host/source in the queue.
-ntpq -p | grep gw.example.com
+NTPQ=$(ntpq -p | grep gw.example.com)
+if [[ $NTPQ == "" ]]; then
+  echo "FAILED: ntpq -p | grep gw.example.com"
+else
+  echo "OK: ntpq -p | grep gw.example.com"
+fi
